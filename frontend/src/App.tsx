a0430ef1e3783276679027099e10f1d0ffc30216
Ipsita Mohanty <ipsita.m@mindfiresolutions.com>
2026-02-05 12:34:19 +0530

    feat(ui): add tailwind config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from './components/ui/button';
-import './App.css';
+import SignupForm from './pages/auth/SignupForm';
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="flex flex-col gap-6">
+      <SignupForm />
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ['class'],
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#13ec80',
+        'primary-dark': '#10bd68',
+        'bg-light': '#f6f8f7',
+        'bg-dark': '#102219',
+        'text-dark': '#111814',
+        'text-light': '#618975',
+        'border-color': '#dbe6e0',
+      },
+      fontFamily: {
+        sans: ['Lexend', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
